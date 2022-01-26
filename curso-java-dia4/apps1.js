79135be5002951a8fdecbc2d7477a2efb8defb8e
Jorge <93343250+jsuazof@users.noreply.github.com>
2022-01-26 09:53:09 -0300

    final ejercicio1
    final ejercicio1

--- curso-java-dia4/apps1.js
+++ curso-java-dia4/apps1.js
@@ -2,7 +2,7 @@ const formulario = document.getElementById("formulario");
 const nombre = document.getElementById("nombre");
 const asunto = document.getElementById("asunto");
 const mensaje = document.getElementById("mensaje");
-const resultado = document.getElementById("resultado");
+let resultado = document.getElementsByClassName("resultado");
 let errorNombre = document.getElementsByClassName("errorNombre");
 let errorAsunto = document.getElementsByClassName("errorAsunto");
 let errorMensaje = document.getElementsByClassName("errorMensaje")
@@ -17,26 +17,24 @@ errorNombre[0].innerHTML = ""
 errorAsunto[0].innerHTML = ""
 errorMensaje[0].innerHTML = ""
 
-const regExpLetras = /^[a-zA-Z]+$/gi;
-const regExpEmail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-
+const regExpLetras = /[a-zA-Z]\w+/gi;
 
 if (!nombre.value.trim()){
    console.log("El nombre es requerido");
-   (errorNombre[0].innerHTML = "El nombre es requerido");
+   errorNombre[0].innerHTML = "El nombre es requerido"
    
 }
+if (!regExpLetras.test(nombre.value)){
+   errorNombre[0].innerHTML = "Solo se permiten letras"
+}
 if (!asunto.value.trim()){
-    console.log("el nombre es requerido");
-   (errorAsunto[0].innerHTML = "El asunto es requerido");
+    errorAsunto[0].innerHTML = "El asunto es requerido"
     
 }
 if (!mensaje.value.trim()){
-    console.log("el nombre es requerido");
-    return (errorMensaje[0].innerHTML = "El mensaje es requerido");
-   }else{
-      (resultado[0].innerHTML = "El mensaje fue enviado");
+    errorMensaje[0].innerHTML = "El mensaje es requerido"
+    return;
    }
-   
-    
+
+      resultado[0].innerHTML = "El mensaje fue enviado con exito"
    });
